6ce32458a7774ee792fd247f4480d6a285673e2e
vg_varankin <skaldnord@yandex.ru>
2024-07-13 23:49:11 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ import (
 
 type Config struct {
 	RunAddress           string `env:"RUN_ADDRESS" envDefault:"localhost:8090"`
-	DatabaseURI          string `env:"DATABASE_DSN"`
+	DatabaseURI          string `env:"DATABASE_URI"`
 	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS" envDefault:"http://localhost:8080"`
 }
 
