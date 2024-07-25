893fb6037e9530d9f1ebd89c4a3e2a8d4f40476e
vg_varankin <skaldnord@yandex.ru>
2024-07-25 18:36:53 +0300

    О_О

--- config/config.go
+++ config/config.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Config struct {
-	RunAddress           string `env:"RUN_ADDRESS" envDefault:":8080"`
+	RunAddress           string `env:"RUN_ADDRESS" envDefault:"localhost:8080"`
 	DatabaseURI          string `env:"DATABASE_URI"`
 	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS" envDefault:"http://localhost:8090"`
 }
